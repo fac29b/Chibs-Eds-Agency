cdf91d7089c3ab16cfd624914dfaf34098bc9850
Chibu <chibsdev@gmail.com>
2023-11-11 17:31:42 +0000

    validation styles added using css and js, submit function yet to be completed

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -155,3 +155,87 @@ function DisplayTeamCards(){
         })
         .catch(error => console.error('Error fetching data:', error));
 };
+
+//Contact Form 
+const form = document.getElementById('contact-form');
+const formName = document.getElementById('name');
+const email = document.getElementById('email');
+const company = document.getElementById('company-name');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+      if (input.value.trim() === '') {
+        showError(input, getCustomErrorMessage(input));
+      } else {
+        showSuccess(input);
+      }
+    });
+  }
+  
+  // Error messages 
+  function getCustomErrorMessage(input) {
+    const customMessages = {
+      'company-name': 'Company name is required, if not applicable type N/A',
+    };
+  
+    return customMessages[input.id] || `${getFieldName(input)} is required`;
+  }
+  
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  checkRequired([company]);
+  checkLength(formName, 3, 15);
+  checkEmail(email);
+});
